5596493ae5f08d4671a76a6a9399b61cc35d47a2
gaudes <ralf@gaudes.net>
2021-03-22 21:25:21 +0100

    Prepare for WebLate translations

--- admin/words.js
+++ admin/words.js
@@ -1,125 +1,15 @@
 /*global systemDictionary:true */
-"use strict";
+'use strict';
 
 systemDictionary = {
-	"boschebike adapter settings": {
-		"en": "Adapter settings for boschebike",
-		"de": "Adaptereinstellungen für boschebike",
-		"ru": "Настройки адаптера для boschebike",
-		"pt": "Configurações do adaptador para boschebike",
-		"nl": "Adapterinstellingen voor boschebike",
-		"fr": "Paramètres d'adaptateur pour boschebike",
-		"it": "Impostazioni dell'adattatore per boschebike",
-		"es": "Ajustes del adaptador para boschebike",
-		"pl": "Ustawienia adaptera dla boschebike",
-		"zh-cn": "boschebike的适配器设置"
-	},
-	"Account":{
-		"en": "Bosch eBike Connect Account",
-		"de": "Bosch eBike Connect Zugangsdaten",
-		"ru": "Учетная запись Bosch eBike Connect",
-		"pt": "Conta Bosch eBike Connect",
-		"nl": "Bosch eBike Connect-account",
-		"fr": "Compte Bosch eBike Connect",
-		"it": "Account Bosch eBike Connect",
-		"es": "Cuenta Bosch eBike Connect",
-		"pl": "Konto Bosch eBike Connect",
-		"zh-cn": "博世eBike Connect帐户"
-	},
-	"General":{
-		"en": "Settings",
-		"de": "Einstellungen",
-		"ru": "настройки",
-		"pt": "Configurações",
-		"nl": "Instellingen",
-		"fr": "Paramètres",
-		"it": "impostazioni",
-		"es": "Configuraciones",
-		"pl": "Ustawienia",
-		"zh-cn": "设定值"
-	},
-	"Informations":{
-		"en": "Additional Informations",
-		"de": "Weitere Informationen",
-		"ru": "Дополнительная информация",
-		"pt": "Informações adicionais",
-		"nl": "Aanvullende informatie",
-		"fr": "Informations supplémentaires",
-		"it": "Informazioni aggiuntive",
-		"es": "Información adicional",
-		"pl": "Dodatkowe informacje",
-		"zh-cn": "附加信息"
-	},
-	"InfoEBike": {
-		"en": "ebike information",
-		"de": "E-Bike-Informationen",
-		"ru": "информация об ebike",
-		"pt": "informações da ebike",
-		"nl": "ebike-informatie",
-		"fr": "informations sur le vélo électrique",
-		"it": "informazioni ebike",
-		"es": "información ebike",
-		"pl": "informacje o e-rowerach",
-		"zh-cn": "电动自行车信息"
-	},
-	"InfoUser": {
-		"en": "User Information",
-		"de": "Nutzerinformation",
-		"ru": "информация о пользователе",
-		"pt": "Informação do usuário",
-		"nl": "gebruikers informatie",
-		"fr": "informations de l'utilisateur",
-		"it": "informazioni utente",
-		"es": "informacion del usuario",
-		"pl": "Informacje o użytkowniku",
-		"zh-cn": "用户信息"
-	},
-	"UpdateInterval":{
-		"en:": "Update interval in minutes",
-		"de": "Aktualisierungsintervall in Minuten",
-		"ru": "Интервал обновления в минутах",
-		"pt": "Intervalo de atualização em minutos",
-		"nl": "Update-interval in minuten",
-		"fr": "Intervalle de mise à jour en minutes",
-		"it": "Intervallo di aggiornamento in minuti",
-		"es": "Intervalo de actualización en minutos",
-		"pl": "Częstotliwość aktualizacji w minutach",
-		"zh-cn": "更新间隔（以分钟为单位）"
-	},
-	"UserName": {
-		"en": "Username (Mailaddress)",
-		"de": "Benutzername (Mailadresse)",
-		"ru": "Имя пользователя (Почтовый адрес)",
-		"pt": "Nome de usuário (endereço de correio)",
-		"nl": "Gebruikersnaam (mailadres)",
-		"fr": "Nom d'utilisateur (adresse e-mail)",
-		"it": "Nome utente (indirizzo e-mail)",
-		"es": "Nombre de usuario (dirección postal)",
-		"pl": "Nazwa użytkownika (adres pocztowy)",
-		"zh-cn": "用户名（邮件地址）"
-	},
-	"UserPassword": {
-		"en": "Password",
-		"de": "Passwort",
-		"ru": "пароль",
-		"pt": "Senha",
-		"nl": "Wachtwoord",
-		"fr": "Mot de passe",
-		"it": "Parola d'ordine",
-		"es": "Contraseña",
-		"pl": "Hasło",
-		"zh-cn": "密码"
-	},
-	"UseSourceAnalytix":{
-		"en": "Use Adapter SourceAnalytix",
-		"de": "Adapter SourceAnalytix verwenden",
-		"ru": "Использовать адаптер SourceAnalytix",
-		"pt": "Use o adaptador SourceAnalytix",
-		"nl": "Gebruik Adapter SourceAnalytix",
-		"fr": "Utiliser l'adaptateur SourceAnalytix",
-		"it": "Usa Adapter SourceAnalytix",
-		"es": "Utilice el adaptador SourceAnalytix",
-		"pl": "Użyj adaptera SourceAnalytix",
-		"zh-cn": "使用适配器SourceAnalytix"
-	}
+    "Account": {                                     "en": "Bosch eBike Connect Account",                     "de": "Bosch eBike Connect Zugangsdaten",                "ru": "Учетная запись Bosch eBike Connect",              "pt": "Conta Bosch eBike Connect",                       "nl": "Bosch eBike Connect-account",                     "fr": "Compte Bosch eBike Connect",                      "it": "Account Bosch eBike Connect",                     "es": "Cuenta Bosch eBike Connect",                      "pl": "Konto Bosch eBike Connect",                       "zh-cn": "博世eBike Connect帐户"},
+    "General": {                                     "en": "Settings",                                        "de": "Einstellungen",                                   "ru": "настройки",                                       "pt": "Configurações",                                   "nl": "Instellingen",                                    "fr": "Paramètres",                                      "it": "impostazioni",                                    "es": "Configuraciones",                                 "pl": "Ustawienia",                                      "zh-cn": "设定值"},
+    "InfoEBike": {                                   "en": "ebike information",                               "de": "E-Bike-Informationen",                            "ru": "информация об ebike",                             "pt": "informações da ebike",                            "nl": "ebike-informatie",                                "fr": "informations sur le vélo électrique",             "it": "informazioni ebike",                              "es": "información ebike",                               "pl": "informacje o e-rowerach",                         "zh-cn": "电动自行车信息"},
+    "InfoUser": {                                    "en": "User Information",                                "de": "Nutzerinformation",                               "ru": "информация о пользователе",                       "pt": "Informação do usuário",                           "nl": "gebruikers informatie",                           "fr": "informations de l'utilisateur",                   "it": "informazioni utente",                             "es": "informacion del usuario",                         "pl": "Informacje o użytkowniku",                        "zh-cn": "用户信息"},
+    "Informations": {                                "en": "Additional Informations",                         "de": "Weitere Informationen",                           "ru": "Дополнительная информация",                       "pt": "Informações adicionais",                          "nl": "Aanvullende informatie",                          "fr": "Informations supplémentaires",                    "it": "Informazioni aggiuntive",                         "es": "Información adicional",                           "pl": "Dodatkowe informacje",                            "zh-cn": "附加信息"},
+    "UpdateInterval": {                              "en": "Update interval in minutes",                      "de": "Aktualisierungsintervall in Minuten",             "ru": "Интервал обновления в минутах",                   "pt": "Intervalo de atualização em minutos",             "nl": "Update-interval in minuten",                      "fr": "Intervalle de mise à jour en minutes",            "it": "Intervallo di aggiornamento in minuti",           "es": "Intervalo de actualización en minutos",           "pl": "Częstotliwość aktualizacji w minutach",           "zh-cn": "更新间隔（以分钟为单位）"},
+    "UseSourceAnalytix": {                           "en": "Use Adapter SourceAnalytix",                      "de": "Adapter SourceAnalytix verwenden",                "ru": "Использовать адаптер SourceAnalytix",             "pt": "Use o adaptador SourceAnalytix",                  "nl": "Gebruik Adapter SourceAnalytix",                  "fr": "Utiliser l'adaptateur SourceAnalytix",            "it": "Usa Adapter SourceAnalytix",                      "es": "Utilice el adaptador SourceAnalytix",             "pl": "Użyj adaptera SourceAnalytix",                    "zh-cn": "使用适配器SourceAnalytix"},
+    "UserName": {                                    "en": "Username (Mailaddress)",                          "de": "Benutzername (Mailadresse)",                      "ru": "Имя пользователя (Почтовый адрес)",               "pt": "Nome de usuário (endereço de correio)",           "nl": "Gebruikersnaam (mailadres)",                      "fr": "Nom d'utilisateur (adresse e-mail)",              "it": "Nome utente (indirizzo e-mail)",                  "es": "Nombre de usuario (dirección postal)",            "pl": "Nazwa użytkownika (adres pocztowy)",              "zh-cn": "用户名（邮件地址）"},
+    "UserPassword": {                                "en": "Password",                                        "de": "Passwort",                                        "ru": "пароль",                                          "pt": "Senha",                                           "nl": "Wachtwoord",                                      "fr": "Mot de passe",                                    "it": "Parola d'ordine",                                 "es": "Contraseña",                                      "pl": "Hasło",                                           "zh-cn": "密码"},
+    "boschebike adapter settings": {                 "en": "Adapter settings for boschebike",                 "de": "Adaptereinstellungen für boschebike",             "ru": "Настройки адаптера для boschebike",               "pt": "Configurações do adaptador para boschebike",      "nl": "Adapterinstellingen voor boschebike",             "fr": "Paramètres d'adaptateur pour boschebike",         "it": "Impostazioni dell'adattatore per boschebike",     "es": "Ajustes del adaptador para boschebike",           "pl": "Ustawienia adaptera dla boschebike",              "zh-cn": "boschebike的适配器设置"},
 };
